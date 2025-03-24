4272fb2f9bf89f3d50b10bbc1dc8a5ed5dade9b8
rapphy243 <36800640+rapphy243@users.noreply.github.com>
2025-03-23 23:04:57 -0500

    Code Review: Sliding Window Implementation Was Wrong
    Recreated Parsing Class w/ 3 different implementations. Example code, old (fixed) sliding window, queue.

--- Essay.java
+++ Essay.java
@@ -14,108 +14,44 @@ public class Essay
     // sequences for the essay.
     private File folder;
     private String fileName;
-    
+
     private int nCount;
     private Set<String> parsedFile;
-    
+
     public Essay(File folder, String fileName, int nCount)
     {
         this.folder = folder;
         this.fileName = fileName;
-        
+
         this.nCount = nCount;
-        this.parsedFile = this.parseFile(getFullPath(), nCount);
+        this.parsedFile = Parsing.listParse(getFullPath(), nCount);
     }
-    
+
     public String getFullPath()
     {
-         return this.folder.toString() + "/" + this.fileName;
+        return this.folder.toString() + "/" + this.fileName;
     }
-    
+
     public File getFolder()
     {
         return this.folder;
     }
-    
+
     public String getFileName()
     {
         return this.fileName;
     }
-    
+
     public int getN()
     {
         return this.nCount;
     }
-    
+
     public Set<String> getParse()
     {
         return this.parsedFile;
     }
-    
-    private Set<String> parseFile(String fullPath, int numWords)
-    {
-        Scanner file;
-        
-        // https://stackoverflow.com/questions/203984/how-do-i-remove-repeated-elements-from-arraylist
-        // tldr use a hashset for no duplicates
-        Set<String> set = new HashSet<>();
-        try {
-            file = new Scanner(new File(fullPath));
-        }
-        catch (java.io.FileNotFoundException fnfe)
-        {
-            System.out.println(fullPath);
-            System.out.println("File path is invalid.");
-            System.out.println("Set will be empty.");
-            return set;
-        }
-        // Faster implementation of parsing file
-        // Create list full of all the stripped words in the file.
-        List<String> wordList = new ArrayList<>();
 
-        // Initialize phrase and count variables to make the first pass of n-contiguous-words
-        int count = 0;
-        String phrase = "";
-        
-        // Iterate through the file and add words to the list and phrases to the set
-        // While doing this create the first pass of n-contiguous-words for the file
-        while (file.hasNext()) 
-        {
-            String word = file.next().replaceAll("[^A-z]", "").toLowerCase(); // Remove all non-alphabetic characters
-            wordList.add(word); // Add word to list
-            if (count < numWords) // If count is less than numWords, add word to phrase
-            {
-                phrase += word;
-                count++;
-            }
-            else // If count is equal to numWords, add phrase to set and reset phrase and count
-            {
-                set.add(phrase);
-                phrase = "";
-                count = 0;
-            }
-        }
-        
-        // Loop through the list of words and create the rest of the n-contiguous-words
-        // Using sliding window technique https://www.geeksforgeeks.org/window-sliding-technique/
-        for (int i = 1; i < numWords; i++) // Our offset
-        {
-            for (int x = i; x <= wordList.size() - numWords; x += numWords) // Our window
-            {
-                phrase = "";
-
-                for (int j = 0; j < numWords; j++) 
-                {
-                    phrase += wordList.get(x + j);
-                }
-
-                set.add(phrase);
-            }
-        }
-
-        return set;
-    }
-    
     public static void main(String[] args)
     {
         // Test to check word phrases from step 0
---
+++ Parsing.java
@@ -0,0 +1,162 @@
+import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
+/**
+ * Write a description of class Parsing here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Parsing
+{
+    public static Set<String> listParse(String fullPath, int numWords)
+    {
+        Scanner file;
+
+        // https://stackoverflow.com/questions/203984/how-do-i-remove-repeated-elements-from-arraylist
+        // tldr use a hashset for no duplicates
+        Set<String> set = new HashSet<String>();
+        try 
+        {
+            file = new Scanner(new File(fullPath));
+        }
+        catch (java.io.FileNotFoundException fnfe)
+        {
+            System.out.println(fullPath);
+            System.out.println("File path is invalid.");
+            System.out.println("Set will be empty.");
+            return set;
+        }
+        // Faster implementation of parsing file
+        // Create list full of all the stripped words in the file.
+        List<String> wordList = new ArrayList<String>();
+
+        // Iterate through the file and add words to the list and phrases to the set
+        // While doing this create the first pass of n-contiguous-words for the file
+        while (file.hasNext()) 
+        {
+            String word = file.next().replaceAll("[^A-z]", "").toLowerCase(); // strip away all punctuation, and set to lowercase
+            wordList.add(word); // Add word to list
+        }
+
+        // Loop through the list of words and create the rest of the n-contiguous-words
+        // Using sliding window technique https://www.geeksforgeeks.org/window-sliding-technique/
+        for (int i = 0; i < numWords; i++) // Our offset
+        {
+            for (int x = i; x <= wordList.size() - numWords; x += numWords) // Our window
+            {
+                String phrase = "";
+                for (int j = 0; j < numWords; j++) 
+                {
+                    phrase += wordList.get(x + j);
+                }
+                set.add(phrase);
+            }
+        }
+
+        return set;
+    }
+    
+    public static Set<String> queueParse(String fullPath, int numWords)
+    {
+        Scanner file;
+
+        // https://stackoverflow.com/questions/203984/how-do-i-remove-repeated-elements-from-arraylist
+        // tldr use a hashset for no duplicates
+        Set<String> set = new HashSet<String>();
+        try 
+        {
+            file = new Scanner(new File(fullPath));
+        }
+        catch (java.io.FileNotFoundException fnfe)
+        {
+            System.out.println(fullPath);
+            System.out.println("File path is invalid.");
+            System.out.println("Set will be empty.");
+            return set;
+        }
+        // Faster implementation of parsing file
+        // Use queue to go through the file once
+
+        Queue<String> queue = new LinkedList<String>();
+
+        while (file.hasNext()) 
+        {
+            String word = file.next().replaceAll("[^A-z]", "").toLowerCase(); // strip away all punctuation, and set to lowercase
+
+            queue.add(word); // add word to queue
+            if (queue.size() == numWords) // if we have numWords size in queue
+            {
+                String phrase = "";
+
+                for (String w : queue) // get each word in queue and make phrase
+                {
+                    phrase += w;
+                }
+                set.add(phrase); // add phrase to queue
+                queue.remove(); // remove oldest word
+            }
+        }                
+        return set;
+    }
+
+    public static Set<String> nLoopParse(String fullPath, int numWords)
+    {
+        Scanner file;
+
+        // https://stackoverflow.com/questions/203984/how-do-i-remove-repeated-elements-from-arraylist
+        // tldr use a hashset for no duplicates
+        Set<String> set = new HashSet<String>();
+
+        for (int i = 0; i < numWords; i++)
+        {
+            try 
+            {
+                file = new Scanner(new File(fullPath));
+            }
+            catch (java.io.FileNotFoundException fnfe)
+            {
+                System.out.println(fullPath);
+                System.out.println("File path is invalid.");
+                System.out.println("Set will be empty.");
+                return set;
+            }
+            for (int x = 0; x < i; x++)
+            {
+                file.next();
+            }
+            nLoopParse(file, set, numWords);
+        }
+        return set;
+    }
+
+    private static void nLoopParse(Scanner file, Set set, int numWords)
+    {
+        while(file.hasNext())
+        {
+            String phrase = "";
+            // read 'numWords' words from file
+            for(int j = 0; j < numWords; j++)
+            {
+                if(file.hasNext())
+                // strip away all punctuation, and set to lowercase
+                    phrase += file.next().replaceAll("[^A-z]","").toLowerCase();
+                else
+                    phrase = null; // not enough words at end of file
+            }
+            if (phrase != null)
+            {
+                set.add(phrase);
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        String path = "./Small number of documents/erk185.shtml.txt";
+        System.out.println("Provided code for parsing size: " + nLoopParse(path,4).size());
+        System.out.println("Window code for parsing size: " + listParse(path,4).size());
+        System.out.println("Queue code for parsing size: " + queueParse(path,4).size());
+    }
+}
